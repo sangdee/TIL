018c317967538fdd865c4babfaf59b7796684d94
sangji06 <tkdwl06@naver.com>
2020-03-27 18:27:25 +0900

    no message

---
+++ testing/src/test.java
@@ -0,0 +1,16 @@
+/**
+ * @author : Sangji Lee
+ * @when : 2020-03-25 오후 5:03
+ * @homepage : https://github.com/sangji11
+ */
+public class test {
+    public static void main(String[] args) {
+
+        double a = 2.11 / 9;
+        double b = 10.55 / 45;
+
+        System.out.println(a == b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
