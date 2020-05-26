66bcf6c057400ed06a983b5071ab011fdc906617
sangji06 <tkdwl06@naver.com>
2020-05-26 23:36:37 +0900

    no message

---
+++ baekjoon/src/_10718.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * @author : Sangji Lee
+ * @when : 2020-05-25 오후 9:04
+ * @homepage : https://github.com/sangji11
+ */
+public class _10718 {
+}
---
+++ baekjoon/src/_10950.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * @author : Sangji Lee
+ * @when : 2020-05-26 오후 11:12
+ * @homepage : https://github.com/sangji11
+ */
+public class _10950 {
+}
