d8d91599d38fb7011e689ae8b53a10fa63533d38
sangji11 <tkdwl06@naver.com>
2020-08-31 22:55:59 +0900

    no message

--- baekjoon/src/UnfinishPalyer.java
+++ baekjoon/src/UnfinishPalyer.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 
 class UnfinishPalyer {
     public static String solution(String[] participant, String[] completion) {
-        String answer;
+        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
-        int i;
         System.out.println(Arrays.toString(participant));
         System.out.println(Arrays.toString(completion));
-        for (i = 0; i < completion.length; i++) {
+        for (int i = 0; i < completion.length; i++) {
             if (!(participant[i].equals(completion[i]))) {
                 answer = participant[i];
                 return answer;
@@ -18,7 +17,7 @@ class UnfinishPalyer {
             }
         }
 
-        return participant[i];
+        return answer;
     }
 
     public static void main(String[] args) {
