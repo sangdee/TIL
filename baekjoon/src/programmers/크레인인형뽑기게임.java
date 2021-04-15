1aee2e159cbe1a72a361c9c89c8476ea16a7e1b5
sj-dev <tkdwl06@naver.com>
2021-04-15 18:21:48 +0900

    2021.04.15

---
+++ baekjoon/src/programmers/크레인인형뽑기게임.java
@@ -0,0 +1,45 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Sangji Lee
+ * @when : 2021-04-15
+ * @github : http://github.com/sangji11
+ */
+public class 크레인인형뽑기게임 {
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < moves.length; i++) {
+            int taget = moves[i] - 1;
+
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[j][taget] != 0) {
+                    list.add(board[j][taget]);
+                    board[j][taget] = 0;
+                    break;
+                }
+            }
+            if (list.size() > 1) {
+                for (int j = 0; j < list.size() - 1; j++) {
+                    if (list.get(j).equals(list.get(j + 1))) {
+                        list.remove(j + 1);
+                        list.remove(j);
+                        answer += 2;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+        solution(board, moves);
+    }
+}
