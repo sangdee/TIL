d0879027b085a91212bd46e68d145497453ef1d8
sangdee <tkdwl06@naver.com>
2021-05-26 12:16:17 +0900

    2021.05.26

---
+++ baekjoon/src/binarySearch/예산.java
@@ -0,0 +1,9 @@
+package binarySearch;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2021-05-26
+ */
+public class 예산 {
+}
---
+++ baekjoon/src/greedy/기지국설치.java
@@ -0,0 +1,25 @@
+package greedy;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2021-05-25
+ */
+public class 기지국설치 {
+    //N	stations	W	answer
+    //11	[4, 11]	1	3
+    public static int solution(int n, int[] stations, int w) {
+        int answer = 3;
+        int[] arr = new int[n + 1];
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3};
+        System.out.println(solution(11, arr, 1));
+    }
+}
