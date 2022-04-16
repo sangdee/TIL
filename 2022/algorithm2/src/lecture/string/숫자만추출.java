e97f2bd9851171114fc9b99e83d86bbf085d4b2b
sangji <tkdwl06@gmail.com>
2022-04-16 15:51:12 +0900

    2022.04.16

---
+++ 2022/algorithm2/src/lecture/string/숫자만추출.java
@@ -0,0 +1,30 @@
+package lecture.string;
+
+import java.util.Scanner;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2022/04/16
+ */
+public class 숫자만추출 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                    result += c;
+            }
+        }
+        int start = 0;
+        for (int i = 0; i < result.length(); i++) {
+            if (result.charAt(i) != '0') {
+                start = i;
+                break;
+            }
+        }
+        System.out.println(result.substring(start));
+    }
+}
---
+++ 2022/test/src/FunctionTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2022/04/16
+ */
+public class FunctionTest {
+
+    @Test
+    void functionTest() {
+
+    }
+}
