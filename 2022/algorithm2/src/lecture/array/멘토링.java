475bbdbcb7d432dcd852bbe41b1b147d5637d149
sangji <tkdwl06@gmail.com>
2022-08-16 22:41:42 +0900

    2022.08.16

---
+++ 2022/algorithm2/src/lecture/array/멘토링.java
@@ -0,0 +1,47 @@
+package lecture.array;
+
+import java.util.Scanner;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2022/08/16
+ */
+public class 멘토링 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[][] arr = new int[m][n];
+
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int count = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int l = 0; l < n; l++) {
+                        if (arr[k][l] == i) {
+                            pi = l;
+                        }
+                        if (arr[k][j] == j) {
+                            pj = l;
+                        }
+                    }
+                    if (pi < pj) {
+                        count++;
+                    }
+                }
+                if (count == m) {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+        
+    }
+}
