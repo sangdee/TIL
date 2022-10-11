657bf0210261ab8dca78f72d35f147506e1489cd
sangji <tkdwl06@gmail.com>
2022-10-11 10:36:30 +0900

    2022.10.11

--- 2022/algorithm2/src/lecture/hash/아나그램.java
+++ 2022/algorithm2/src/lecture/hash/아나그램.java
@@ -1,5 +1,6 @@
 package lecture.hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
@@ -10,7 +11,6 @@ import java.util.Scanner;
  * @since : 2022/10/11
  */
 public class 아나그램 {
-
     public static String solution(String word, String secondWord) {
         HashMap<Character, Integer> map1 = new HashMap<>();
         HashMap<Character, Integer> map2 = new HashMap<>();
@@ -26,15 +26,28 @@ public class 아나그램 {
         for (Entry<Character, Integer> entry : map1.entrySet()) {
             if (!map2.get(entry.getKey()).equals(entry.getValue())) {
                 result = "NO";
+                break;
             }
         }
         return result;
+    }
+    public static String isAnagram(String str1, String str2) {
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        if (Arrays.equals(charArray1, charArray2)) {
+            return "YES";
+        }
+        return "NO";
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
         String secondWord = sc.nextLine();
-        System.out.println(solution(word, secondWord));
+        System.out.println(isAnagram(word, secondWord));
     }
 }
