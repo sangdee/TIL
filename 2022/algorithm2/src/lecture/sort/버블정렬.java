1f4a3736318df1ee3770db81323fa84dbd9a9ec9
sangji <tkdwl06@gmail.com>
2022-11-28 17:04:38 +0900

    2022.11.28

---
+++ 2022/algorithm2/src/lecture/sort/버블정렬.java
@@ -0,0 +1,33 @@
+package lecture.sort;
+
+import java.util.Scanner;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2022/11/28
+ */
+public class 버블정렬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
---
+++ 2022/algorithm2/src/lecture/sort/선택정렬.java
@@ -0,0 +1,31 @@
+package lecture.sort;
+
+import java.util.Scanner;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2022/11/28
+ */
+public class 선택정렬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
