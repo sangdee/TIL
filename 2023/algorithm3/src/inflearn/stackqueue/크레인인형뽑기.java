b9c01f847a575f0d1a53f24f18b8dc60a6fc6a7b
sangdee <tkdwl06@gmail.com>
2023-05-14 16:27:40 +0900

    2023.05.14

---
+++ 2023/algorithm3/src/inflearn/stackqueue/크레인인형뽑기.java
@@ -0,0 +1,52 @@
+package inflearn.stackqueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2023/05/14
+ */
+public class 크레인인형뽑기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) {
+            moves[i] = sc.nextInt();
+        }
+        solution(board, moves);
+    }
+
+    private static void solution(int[][] board, int[] moves) {
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for (int i = 0; i < moves.length; i++) {
+            int move = moves[i] - 1;
+            for (int j = 0; j < board.length; j++) {
+                int doll = board[j][move];
+                if (doll != 0) {
+                    if (!stack.isEmpty() && stack.peek() == doll) {
+                        stack.pop();
+                        count++;
+                    } else {
+                        stack.add(doll);
+                    }
+                    board[j][move] = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println(count * 2);
+    }
+}
---
+++ 2023/algorithm3/src/inflearn/stackqueue/후위연산식.java
@@ -0,0 +1,42 @@
+package inflearn.stackqueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2023/05/14
+ */
+public class 후위연산식 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                stack.add(Integer.parseInt(String.valueOf(c)));
+            } else {
+                Integer calculatedValue = calculate(stack, c);
+                stack.add(calculatedValue);
+            }
+        }
+        System.out.println(stack.pop());
+    }
+
+    private static Integer calculate(Stack<Integer> stack, char c) {
+        Integer firstOperand = stack.pop();
+        Integer secondOperand = stack.pop();
+        if (c == '+') {
+            return secondOperand + firstOperand;
+        } else if (c == '-') {
+            return secondOperand - firstOperand;
+        } else if (c == '*') {
+            return secondOperand * firstOperand;
+        } else {
+            return secondOperand / firstOperand;
+        }
+    }
+}
