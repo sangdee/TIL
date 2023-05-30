9c2bf916696abf402ff3e9cc09b71a0215c4d546
sangdee <tkdwl06@gmail.com>
2023-05-30 14:07:06 +0900

    2023.05.30

---
+++ practice/src/designpatterns/proxy/CommonProxy.java
@@ -0,0 +1,43 @@
+package designpatterns.proxy;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2023/05/30
+ */
+public class CommonProxy implements BookService, OfferService {
+    private final BookService bookService;
+    private final OfferService offerService;
+
+    public CommonProxy(BookService bookService) {
+        this.bookService = bookService;
+        this.offerService = null;
+    }
+
+    public CommonProxy(OfferService offerService) {
+        this.bookService = null;
+        this.offerService = offerService;
+    }
+
+    @Override
+    public void offer(Offer offer) {
+        transaction();
+        offerService.offer(offer);
+        log();
+    }
+
+    @Override
+    public void rent(Book book) {
+        transaction();
+        bookService.rent(book);
+        log();
+    }
+
+    private void log() {
+        System.out.println("로그");
+    }
+
+    private void transaction() {
+        System.out.println("트랜잭션");
+    }
+}
---
+++ practice/src/designpatterns/proxy/Offer.java
@@ -0,0 +1,18 @@
+package designpatterns.proxy;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2023/05/30
+ */
+public class Offer {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Offer(String name) {
+        this.name = name;
+    }
+}
