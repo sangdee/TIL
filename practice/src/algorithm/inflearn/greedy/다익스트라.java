fd2999058a6b50ac03bd9df41eb6b2d89f2c3270
sangdee <tkdwl06@gmail.com>
2023-07-07 10:13:25 +0900

    2023.07.07

---
+++ practice/src/algorithm/inflearn/greedy/다익스트라.java
@@ -0,0 +1,79 @@
+package algorithm.inflearn.greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * @author : Sangji Lee
+ * @see : https://github.com/sangdee
+ * @since : 2023/07/07
+ */
+public class 다익스트라 {
+    static int n, m;
+    static ArrayList<ArrayList<Edge>> graph;
+    static int[] dis;
+
+    private static void solution(int v) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(v, 0));
+        dis[v] = 0;
+        while (!pq.isEmpty()) {
+            Edge tmp = pq.poll();
+            int now = tmp.vex;
+            int nowCost = tmp.cost;
+            if (nowCost > dis[now]) {
+                continue;
+            }
+            for (Edge ob : graph.get(now)) {
+                if (dis[ob.vex] > nowCost + ob.cost) {
+                    dis[ob.vex] = nowCost + ob.cost;
+                    pq.offer(new Edge(ob.vex, nowCost + ob.cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList<ArrayList<Edge>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Edge>());
+        }
+        dis = new int[n + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(a).add(new Edge(b, c));
+        }
+        solution(1);
+        for (int i = 2; i <= n; i++) {
+            if (dis[i] != Integer.MAX_VALUE) {
+                System.out.println(i + " : " + dis[i]);
+            } else {
+                System.out.println(i + " : impossible");
+            }
+        }
+    }
+
+
+    static class Edge implements Comparable<Edge> {
+        int vex;
+        int cost;
+
+        public Edge(int vex, int cost) {
+            this.vex = vex;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+}
