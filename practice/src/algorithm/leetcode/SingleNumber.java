a19e480c8290f7e8734753638c426688ad8aad9f
sangji <tkdwl06@gmail.com>
2024-08-13 10:56:07 +0900

    2024.08.13

---
+++ practice/src/algorithm/leetcode/SingleNumber.java
@@ -0,0 +1,20 @@
+package algorithm.leetcode;
+
+import java.util.HashMap;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) == 1) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
