ff0bada7712ba6faa1a7a711d4790c6d477f86f5
sangji <tkdwl06@gmail.com>
2024-08-27 13:43:27 +0900

    2024.08.27

---
+++ practice/src/algorithm/leetcode/LinkedListCycle.java
@@ -0,0 +1,22 @@
+package algorithm.leetcode;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
